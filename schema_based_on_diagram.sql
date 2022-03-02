a5fd93ab2891db58b3f4d32a50d5191173b8e43e
Anuar Shaidenov <anuarshaidenov@gmail.com>
2022-03-02 15:17:04 +0600

    Add relations to the tables

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -2,6 +2,7 @@ CREATE TABLE patients(
 id INT GENERATED ALWAYS AS IDENTITY,
 name VARCHAR,
 date_of_birth date,
+PRIMARY KEY (id)
 );
 
 CREATE TABLE invoices(
@@ -10,6 +11,8 @@ total_amount DECIMAL,
 generated_at TIMESTAMP,
 payed_at TIMESTAMP,
 medical_history_id INT,
+PRIMARY KEY (id),
+FOREIGN KEY (medical_history_id) REFERENCES medical_history (id)
 );
 
 CREATE TABLE medical_history(
@@ -17,6 +20,8 @@ id INT GENERATED ALWAYS AS IDENTITY,
 admitted_at TIMESTAMP,
 patient_id INT,
 status VARCHAR,
+PRIMARY KEY (id),
+FOREIGN KEY (patient_id) REFERENCES patients (id)
 );
 
 CREATE TABLE invoice_items(
@@ -26,6 +31,9 @@ quantity INT,
 total_price DECIMAL,
 invoice_id INT,
 treatment_id INT,
+PRIMARY KEY (id),
+FOREIGN KEY (invoice_id) REFERENCES invoices (id),
+FOREIGN KEY (treatment_id) REFERENCES treatments (id)
 );
 
 
@@ -33,4 +41,14 @@ CREATE TABLE treatments(
 id INT GENERATED ALWAYS AS IDENTITY,
 type VARCHAR,
 name VARCHAR,
+PRIMARY KEY (id)
+);
+
+CREATE TABLE treatments_histories (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    treatment_id INT,
+    medical_history_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (treatment_id) REFERENCES treatments (id),
+    FOREIGN KEY (medical_history_id) REFERENCES medical_history (id)
 );
