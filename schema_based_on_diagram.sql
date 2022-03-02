4ef84a92cf5f188265f6aa7b5f7e528d43139a7e
Anuar Shaidenov <anuarshaidenov@gmail.com>
2022-03-02 18:44:41 +0600

    Create remaining indexes

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -15,6 +15,8 @@ PRIMARY KEY (id),
 FOREIGN KEY (medical_histories_id) REFERENCES medical_histories (id)
 );
 
+CREATE INDEX medical_histories_index ON invoices (medical_histories_id);
+
 CREATE TABLE medical_histories(
 id INT GENERATED ALWAYS AS IDENTITY,
 admitted_at TIMESTAMP,
@@ -23,6 +25,9 @@ status VARCHAR(255),
 PRIMARY KEY (id),
 FOREIGN KEY (patient_id) REFERENCES patients (id)
 );
+
+CREATE INDEX patient_index ON medical_histories (patient_id);
+
 
 CREATE TABLE invoice_items(
 id INT GENERATED ALWAYS AS IDENTITY,
@@ -35,6 +40,9 @@ PRIMARY KEY (id),
 FOREIGN KEY (invoice_id) REFERENCES invoices (id),
 FOREIGN KEY (treatment_id) REFERENCES treatments (id)
 );
+
+CREATE INDEX treatment_invoice_index ON invoice_items (treatment_id);
+CREATE INDEX invoice_index ON invoice_items (invoice_id);
 
 
 CREATE TABLE treatments(
